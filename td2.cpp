2857bd6922f714debae3f2b7b853f1e19699c911
Manuel Jarry <manuel.jarry@polymtl.ca>
2025-02-11 22:00:54 -0500

    fix[partie1]:Removed a 'delete' line that prevented the program's complete execution

--- td2.cpp
+++ td2.cpp
@@ -135,7 +135,6 @@ Acteur* lireActeur(istream& fichier)
 	acteur->nom = nomActeur;
 	acteur->anneeNaissance = lireUint16(fichier);
 	acteur->sexe = lireUint8(fichier);
-	delete[] acteur->joueDans.elements;
 	acteur->joueDans = acteurListesFilms;
 	return acteur;//TODO: Retourner un pointeur soit vers un acteur existant ou un nouvel acteur ayant les bonnes informations, selon si l'acteur existait déjà.  Pour fins de débogage, affichez les noms des acteurs crées; vous ne devriez pas voir le même nom d'acteur affiché deux fois pour la création.
 }
